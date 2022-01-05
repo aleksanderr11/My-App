0ab58c990badf12643b2d2cbe59122fdc9736c20
aleksanderr11 <aleksradzi@gmail.com>
2022-01-05 23:30:22 +0100

    Zadanie 3 i 4: dodaj pensje

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -4,6 +4,7 @@ public class Human {
 
     Animal pet;
     Car car;
+    private Double salary;
 
     public Animal getPet() {
         return pet;
@@ -19,5 +20,21 @@ public class Human {
 
     public void setCar(Car car) {
         this.car = car;
+    }
+
+    public Double getSalary() {
+        System.out.println("Actual salary: "+this.salary);
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        if(salary <= 0){
+            System.out.println("Salary should be more than 0");
+            return;
+        }
+        System.out.println("New salary value: "+salary);
+        System.out.println("Please contact HR departament to pick up contract amendment");
+        System.out.println("Information about your new salary has been send to ZUS and US");
+        this.salary = salary;
     }
 }
