2a315bf8d0cf4c5256d3a4dcb78276b5dd7d6b44
aleksanderr11 <aleksradzi@gmail.com>
2022-01-05 22:46:29 +0100

    Zadanie 1: dodaj karmienie, spacer

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -1,4 +1,46 @@
 package com.company;
 
 public class Animal {
+
+    final String species;
+    private Double weight;
+    private boolean isDead;
+
+    public Animal(String species) {
+        this.species = species;
+        if (species.equals("felis")) {
+            this.weight = 1.5;
+        } else if (species.equals("canis")) {
+            this.weight = 2.0;
+        } else if (species.equals("aves")) {
+            this.weight = 0.4;
+        } else if (species.equals("lacertilia")) {
+            this.weight = 0.1;
+        } else {
+            this.weight = 1.0;
+        }
+    }
+
+    void feed() {
+        if(isDead){
+            System.out.println("Animal is dead");
+            return;
+        }
+        this.weight += 0.1;
+    }
+
+    void takeForAWalk() {
+        if(isDead){
+            System.out.println("Animal is dead");
+            return;
+        }
+        this.weight-= 0.1;
+        if(this.weight <= 0){
+            isDead = true;
+        }
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,24 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Animal animal1 = new Animal("lacertilia");
+        System.out.println("Start weight "+animal1.getWeight());
+        animal1.feed();
+        System.out.println("Feed 1 "+animal1.getWeight());
+        animal1.feed();
+        System.out.println("Feed 2 "+animal1.getWeight());
+        animal1.takeForAWalk();
+        System.out.println("Walk 1 "+animal1.getWeight());
+        animal1.takeForAWalk();
+        System.out.println("Walk 2 "+animal1.getWeight());
+        animal1.takeForAWalk();
+        System.out.println("Walk 3 "+animal1.getWeight());
+        animal1.takeForAWalk();
+        System.out.println("Walk 4 "+animal1.getWeight());
+        animal1.takeForAWalk();
+        animal1.feed();
+
+
     }
 }
